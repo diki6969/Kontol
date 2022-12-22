8b62cc95e023fa296545897e33388aecf866a3c8
diki6969 <120658441+diki6969@users.noreply.github.com>
2022-12-22 21:47:13 +0000

    Juhu

--- replit.nix
+++ replit.nix
@@ -1,10 +1,19 @@
 { pkgs }: {
     deps = [
         pkgs.nodejs
+        pkgs.nodejs
+        pkgs.nodejs-16_x
+        pkgs.nodejs-16_x
+        pkgs.nodejs-16_x
+        pkgs.nodejs-16_x
+        pkgs.nodejs-16_x
+        pkgs.nodejs-16_x
+        pkgs.nodejs
         pkgs.nodePackages.typescript
         pkgs.ffmpeg
         pkgs.imagemagick
         pkgs.git
         pkgs.libwebp
+        pkgs.npm
     ];
 }
